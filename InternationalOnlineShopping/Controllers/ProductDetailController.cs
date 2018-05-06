237c5b935cc2549f6a63c876d7bfe8e80af563ce
sukheed <da_sukhee@yahoo.com>
2018-05-06 12:16:08 -0500

    These are changes in the following files (work in progress): ProductDetailController AdminLayout View ProductDetail cshtml Web.config

---
+++ InternationalOnlineShopping/Controllers/ProductDetailController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InternationalOnlineShopping.Models;
+using InternationalOnlineShopping.Repository;
+
+namespace InternationalOnlineShopping.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public GenericUnitOfWork unitOfWork = new GenericUnitOfWork();
+        public ActionResult ProductDetail(int pId)
+        {
+            System.Diagnostics.Debug.WriteLine("---------");
+            Product pd = unitOfWork.GetRepositoryInstance<Product>().GetFirstOrDefault(pId);
+            ViewBag.SimilarProducts = unitOfWork.GetRepositoryInstance<Product>().GetListByParameter(i => i.CategoryId == pd.CategoryId).ToList();
+            return View(pd);
+        }
+    }
+}
